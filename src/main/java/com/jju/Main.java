aaecec48d23a72f5c493fd3502ee83dd65edc1d1
bilo780101-cell <bilo780101@gmail.com>
2026-04-13 22:57:30 +0300

    Assignment Completed

---
+++ src/main/java/com/jju/Main.java
@@ -0,0 +1,18 @@
+package com.jju;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<PaymentMethod> payments = new ArrayList<>();
+
+        payments.add(new CreditCard("1234-5678-9012-3456"));
+        payments.add(new PayPal("hamduedris99@gmail.com"));
+
+        for (PaymentMethod payment : payments) {
+            payment.processPayment(99.99);
+        }
+    }
+}
